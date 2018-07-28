cb6860a9ee32354baf52fb0ced5e7cefdfa46c43
Balaji <jijodv@gmail.com>
2018-07-28 15:15:37 +0800

    Updates

--- python/findS_B_pos_funct.py
+++ python/findS_B_pos_funct.py
@@ -6,15 +6,16 @@
 
 import os, commands
 
-
+print
 print "Enter 5 Numbers"
-a=input("Number 0: ")
-b=input("Number 1: ")
-c=input("Number 2: ")
-d=input("Number 3: ")
-e=input("Number 4: ")
+N0=input("Number 0: ")
+N1=input("Number 1: ")
+N2=input("Number 2: ")
+N3=input("Number 3: ")
+N4=input("Number 4: ")
 
-ARR=[a,b,c,d,e]
+
+ARR=[N0, N1, N2, N3, N4]
 B_N=max(ARR)
 B_P=ARR.index(B_N)
 S_N=min(ARR)
--- python/findS_B_pos_ownlogic.py
+++ python/findS_B_pos_ownlogic.py
@@ -5,14 +5,14 @@ import os, commands
 
 
 print "Enter 5 Numbers"
+N0=input("Number 0: ")
 N1=input("Number 1: ")
 N2=input("Number 2: ")
 N3=input("Number 3: ")
 N4=input("Number 4: ")
-N5=input("Number 5: ")
 
-ARR=[N1, N2, N3, N4, N5]
-B_INT=N1
+ARR=[N0, N1, N2, N3, N4]
+B_INT=N0
 for B_LST in ARR:
 	if B_LST > B_INT:
 		B_INT = B_LST
@@ -23,11 +23,11 @@ print"Biggest in the Array is: ", B_INT
 print"Position of the biggest number: ", B_ARR_P
 print
 
-S_INT=N1
+S_INT=N0
 for S_LST in ARR:
 	if S_LST < S_INT:
 		S_INT = S_LST
-		S_ARR_P=ARR.index(S_INT)
+		S_ARR_P = ARR.index(S_INT)
 print
 print"Smallest in the Array is: ", S_INT
 print"Position of the smallest number: ", S_ARR_P
